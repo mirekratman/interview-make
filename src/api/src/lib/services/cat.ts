9ce76664f07dabe04fd17ee29f0613a0d17f7b53
Mirek Ratman <mirek.ratman@gmail.com>
2024-04-11 16:22:09 +0200

    simple JSON cats service

---
+++ src/api/src/lib/services/cat.ts
@@ -0,0 +1,8 @@
+import { JsonDB, Config } from 'node-json-db';
+
+// INFO - second true replace with false to disable human readable format
+var catService = new JsonDB(
+    new Config(process.env.API_JSON_DB as string, true, true, '/')
+);
+
+export default catService;
