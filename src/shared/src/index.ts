a0e49d6cf12ba6df7ab69f4e595d2fe9f3b38ab9
Mirek Ratman <mirek.ratman@gmail.com>
2024-04-11 16:23:07 +0200

    shared internal repo

---
+++ src/shared/src/index.ts
@@ -0,0 +1,20 @@
+import {
+    Cat,
+    CatsResponse,
+    RandomCatResponse,
+    RateCatPayload,
+    RateCatResponse,
+    ListTopCats,
+} from './interfaces/cat';
+
+import { ErrorResponse } from './interfaces/error';
+
+export {
+    Cat,
+    CatsResponse,
+    RandomCatResponse,
+    RateCatPayload,
+    RateCatResponse,
+    ListTopCats,
+    ErrorResponse,
+};
---
+++ src/shared/src/interfaces/cat.ts
@@ -0,0 +1,30 @@
+/*
+ Cat interfaces
+ */
+export interface Cat {
+    id: string;
+    image: string;
+    title: string;
+    date: Date;
+    rating: number;
+    rating_count: number;
+    rating_sum: number;
+}
+// INFO - make nonsense to use key cats. Just return array of cats
+export interface CatsResponse {
+    cats: Array<Cat>;
+}
+export interface RandomCatResponse extends Cat {}
+// TODO naming (used in FE)
+export interface ListTopCats extends CatsResponse {}
+
+/*
+ Rate interfaces
+ */
+export interface RateCatPayload {
+    id: string;
+    rating: number;
+}
+export interface RateCatResponse extends Cat {
+    success: boolean;
+}
