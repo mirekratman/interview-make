59fa6b60900d4836a657859a77d964dac0a9dbb8
Mirek Ratman <mirek.ratman@gmail.com>
2024-04-11 16:21:35 +0200

    endpoint for error test

--- src/api/src/lib/routes/api.ts
+++ src/api/src/lib/routes/api.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import { ErrorResponse } from '@make/shared';
 
 // TODO This can be done as post build step too. But for now, I will just import the package.json file
 import packageJson from '../../../package.json';
@@ -7,12 +8,27 @@ import packageJson from '../../../package.json';
 const apiRoutes = express.Router();
 
 // Handle API root route
-// TODO add types
-apiRoutes.get('/', (req, res) => {
+apiRoutes.get('/', (req: Request, res: Response) => {
     res.send(`API v${packageJson.version}`);
 });
 
-// TODO extend and fix this lates
+// Handle test error route
+apiRoutes.get('/testerror', (req: Request, res: Response) => {
+    try {
+        throw new Error('Test error');
+    } catch (err: any) {
+        console.error(err);
+        const error = {
+            message: err?.message,
+            code: '500',
+        } as ErrorResponse;
+
+        res.send(error);
+    }
+});
+
+// TODO extend and fix this later
+// TODO ched if its needed
 apiRoutes.use(
     '/api/upload',
     (req: Request, res: Response, next: NextFunction) => {
