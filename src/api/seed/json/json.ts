bc97c05849d8c9a9aa0bff7ee74d706c120c213d
Mirek Ratman <mirek.ratman@gmail.com>
2024-04-18 04:39:15 +0200

    seed updates

--- src/api/seed/json/json.ts
+++ src/api/seed/json/json.ts
@@ -1,16 +1,23 @@
-import CatService from '../../src/lib/services/cat';
-import uuid4 = require('uuid4');
+import uuid4 from 'uuid4';
 import { Cat } from '@make/shared';
+import { JsonDB, Config } from 'node-json-db';
 
 export const seedJson = async () => {
+    var CatService = new JsonDB(
+        new Config(String(process.env.API_JSON_DB), true, true, '/')
+    );
+
     console.log('Seeding JSON data');
     const content = await CatService.getData(`/`);
+
+    // INFO - Seed uses same photo for all cats for testing purposes
+    // INFO - This could be extended to use different photos for each cat
 
     if (Object.keys(content)?.length <= 0) {
         // Add one predefined record for tests
         await CatService.push(`/${String(process.env.API_TEST_UUID)}`, <Cat>{
             id: String(process.env.API_TEST_UUID),
-            image: 'https://i.pravatar.cc/300',
+            images: ['31df4056e24b198c9fb769ae026e95c8'], // INFO - same fileid for testing purposes
             title: `Cat test`,
             date: new Date(),
             rating: 0,
@@ -24,7 +31,7 @@ export const seedJson = async () => {
 
             await CatService.push(`/${id}`, <Cat>{
                 id: id,
-                image: 'https://i.pravatar.cc/300',
+                images: ['31df4056e24b198c9fb769ae026e95c8'], // INFO - same fileid for testing purposes
                 title: `Cat ${i}`,
                 date: new Date(),
                 rating: Math.floor(Math.random() * 5),
--- src/api/seed/sql/sql.ts
+++ src/api/seed/sql/sql.ts
@@ -1,36 +1,49 @@
 import fs from 'fs';
-import pg from 'pg';
+import pg, { Pool } from 'pg';
 
-const dbPool = new pg.Pool({
-    host: String(process.env.POSTGRES_HOST),
-    port: Number(process.env.POSTGRES_PORT),
-    user: String(process.env.POSTGRES_USER),
-    password: String(process.env.POSTGRES_PASSWORD),
-    database: String(process.env.POSTGRES_DATABASE),
-});
+let dbPool: Pool | undefined = undefined;
 
-// TODO add types
+// INFO - Seed fails on connection terminated error
+// INFO - could be related to https://github.com/knex/knex/issues/3523
+// INFO - As Im using M1 - this could be related to https://github.com/knex/knex/issues/3523#issuecomment-879809988
+const init = () => {
+    if (!dbPool) {
+        dbPool = new pg.Pool({
+            host: String(process.env.POSTGRES_HOST),
+            port: Number(process.env.POSTGRES_PORT),
+            user: String(process.env.POSTGRES_USER),
+            password: String(process.env.POSTGRES_PASSWORD),
+            database: String(process.env.POSTGRES_DATABASE),
+            min: 0,
+            max: 5,
+            idleTimeoutMillis: 600000,
+        });
+    }
+
+    return dbPool;
+};
+
 export const seedDb = async () => {
     console.log('Seeding SQL structure');
+    const pool = init();
 
     // INFO - could be done async but sql files are not big and do not rquire different approach
-    // TODO - check lints
     const seedData = fs.readFileSync('./seed/sql/dump.sql').toString();
 
     if (seedData) {
         // TODO - fix connection terminated issue - Error: Connection terminated unexpectedly
-        //await dbPool.query(seedData);
+        //await pool.query(seedData);
     }
 };
 
 export const seedContent = async () => {
     console.log('Seeding SQL content');
+    const pool = init();
 
     // INFO - could be done async but sql files are not big and do not rquire different approach
-    // TODO - check lints
     const seedData = fs.readFileSync('./seed/sql/content.sql').toString();
 
     if (seedData) {
-        //await dbPool.query(seedData);
+        //await pool.query(seedData);
     }
 };
