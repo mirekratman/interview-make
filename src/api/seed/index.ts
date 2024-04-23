c55a753bda8d308e3a76ffc2a149bd4aa96d09d5
Mirek Ratman <mirek.ratman@gmail.com>
2024-04-23 19:00:12 +0200

    simplify DB seed

--- src/api/seed/index.ts
+++ src/api/seed/index.ts
@@ -1,10 +1,10 @@
 import { seedJson } from './json/json';
-import { seedDb, seedContent } from './sql/sql';
+import { seedDb } from './sql/sql';
 
 // Seed all necessary data
 // INFO - we can extend loggind and error handling but more detailed spec needed.
 const init = async () => {
-    Promise.all([seedJson(), seedDb(), seedContent()]);
+    Promise.all([seedJson(), seedDb()]);
 };
 
 init();
